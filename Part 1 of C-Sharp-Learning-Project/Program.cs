819c985b8d495a832f80aa034b2e1d4e0ca7c6cf
Razvan <razvan.mierla13@gmail.com>
2021-11-26 22:34:01 +0200

    Made a small test timer

--- Part 1 of C-Sharp-Learning-Project/Program.cs
+++ Part 1 of C-Sharp-Learning-Project/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class TestClass
+{
+    private static System.Timers.Timer Timer;
+
+    static void Main()
+    {
+        CreateTimer();
+
+        Console.WriteLine("Press enter to stop app...");
+        Console.WriteLine("App started at {0:HH:mm:ss.fff}", DateTime.Now);
+        Console.ReadLine();
+
+        Timer.Stop();
+        Timer.Dispose();
+
+        Console.WriteLine("Exiting the app...");
+    }
+
+    private static void CreateTimer()
+    {
+        Timer = new System.Timers.Timer(2000);
+
+        Timer.Elapsed += Timer_Elapsed;
+        Timer.AutoReset = true;
+        Timer.Enabled = true;
+    }
+
+    private static void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        Console.WriteLine("Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
+    }
+}
